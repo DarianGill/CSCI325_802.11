9b54c34a1f0c54b4811cc3027f5fe6c4a5e3394c
Kyle Muir <kyle.c.muir@gmail.com>
2019-11-09 15:43:23 -0800

    Changes as of 11.9.19

--- WiFiProject/src/wifi/Receiver.java
+++ WiFiProject/src/wifi/Receiver.java
@@ -1,7 +1,21 @@
 package wifi;
 
+/**
+ * Receiver will monitor the network for incoming data, which it will remove from it's packet and store in 
+ * an ArrayBlockingQueue until the data is requested by the client layer. This class is also responsible for
+ * handling ACKs after receiving data.
+ * 
+ * @author Kyle Muir
+ * @version 11.3.19
+ *
+ */
 public class Receiver implements Runnable {
 	
+	/**
+	 * 
+	 * 
+	 */
+	@Override
 	public void run() {
 		//TODO
 	}
